4be4774c53f8ee1778e7e4260cdd7b6c925d9274
alfathaniman <alfathan.iman@gmail.com>
2020-02-23 21:03:08 +0700

    second push

---
+++ public/app/app.js
@@ -0,0 +1,1 @@
+var app = angular.module('customerRecords', []).constant('API_URL', 'http://127.0.0.1:8000/api/v1/');
---
+++ resources/js/app.js
@@ -0,0 +1,2 @@
+require('./bootstrap');
+var app = angular.module('customersController', []).constant('API_URL', 'http://localhost:8000/api/v1/');
